dc16fd0189a42b69c8adb58eaa32a4dc16a6fb8e
Eramood <122992858+Eramood@users.noreply.github.com>
2025-02-13 15:48:12 +0100

    first commit

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+<section class="hero-section default-margin">
+    <h3>Hello! I'm Marco</h3>
+    <div class="underscore mb-m"></div>
+    <h1>I'm a Svelte developer crafting intuitive and performant</h1>
+    <h1 class="dark-grey mb-m">web experiences.</h1>
+  </section>
