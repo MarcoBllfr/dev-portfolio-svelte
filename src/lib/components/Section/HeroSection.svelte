068d22a0ff1eb3b45222884d8cd8330f5b2e1714
Eramood <122992858+Eramood@users.noreply.github.com>
2025-02-24 16:53:00 +0100

    Create the main section (Hero and About me), and upload assets

--- src/lib/components/Section/HeroSection.svelte
+++ src/lib/components/Section/HeroSection.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
+  import { Button } from "$components";
+   
+  function onclick(){
+    goto("/#contact-form");
+  }
 
 </script>
 <section class="hero-section default-margin">
@@ -6,4 +12,10 @@
     <div class="underscore mb-m"></div>
     <h1>I'm a Svelte developer crafting intuitive and performant</h1>
     <h1 class="dark-grey mb-m">web experiences.</h1>
+    <Button {onclick}>Let's talk</Button>
   </section>
+  <style>
+    .hero-section{
+      padding-top: 60px;
+    }
+  </style>
