24fffeb36e38f3b7a9d8b1f1b8611b2e0c4db871
Eramood <122992858+Eramood@users.noreply.github.com>
2025-03-05 18:08:20 +0100

    Renderinf of blog articles

---
+++ src/routes/work/[slug]/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    let {data} = $props();
+    let {company, name, dateAccomplished, stack, pojectImageUrl, content} = data.project;
+    function getTagFromStyle(style : ProcessedTextContent["style"]):string {
+        if (style === "normal"){
+            return "p"
+        }else{
+            return style;
+        }
+    }
+</script>
+<main class="default-margin work-page">
+<h4>{company}</h4>
+<div class="underscore"></div>
+<h2 class="mb-s">{name}</h2>
+<img class="project-image" src={pojectImageUrl} alt=""/>
+<div class="project-container mt-m">
+    <div class="meta-data">
+        <h3 class="semi-bold">Date</h3>
+        <p>{dateAccomplished.slice(0,7)}</p>
+        <h3 class="semi-bold mt-m">Tech Stack</h3>
+        <ul>
+            {#each stack as skill }
+                <li>{skill}</li>
+            {/each}
+        </ul>
+    </div>
+    <div class="project-text">
+        {#each content as block }
+            {#if block.type==="text"}
+                <svelte:element this={getTagFromStyle(block.style)}>
+                    {block.textToRender}
+                </svelte:element>
+                {:else}
+                <img class="content-img" src={block.url} alt="" />
+                {/if}
+        {/each}
+    </div>
+</div>
+</main>
